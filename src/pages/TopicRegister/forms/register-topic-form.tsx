4e8519d2f0c9d24e2fa873bd540cd38509713a7a
https://github.com/HurasAdam <huradam@wp.pl>
2025-06-30 01:05:06 +0200

    feat: add support registering conversation topics.

--- src/pages/TopicRegister/forms/register-topic-form.tsx
+++ src/pages/TopicRegister/forms/register-topic-form.tsx
@@ -1,76 +1,203 @@
-import { CircleCheck, Mail, Phone } from "lucide-react";
-import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CircleCheck, Loader2, Mail, Phone } from "lucide-react";
+import { useState, type JSX } from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import type z from "zod";
 import { Button } from "../../../components/ui/button";
+import { DialogFooter } from "../../../components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../../components/ui/form";
 import { Input } from "../../../components/ui/input";
+import { Textarea } from "../../../components/ui/textarea";
+import { useCreateReportTopicMutation } from "../../../hooks/report-topic/use-report-topic";
+import type { ITopic } from "../../../types/topic";
+import { reportTopicSchema } from "../../../validation/report-topic.schema";
 
-const RegisterTopicForm = ({ topic }) => {
+export type reportTopicForm = z.infer<typeof reportTopicSchema>;
+
+interface IRegisterTopicFormProps {
+  topic: ITopic;
+}
+
+const RegisterTopicForm = ({ topic }: IRegisterTopicFormProps) => {
   const [feedback, setFeedback] = useState<null | "call" | "message">(null);
-
-  const { register, handleSubmit, reset } = useForm({
+  const [loadingType, setLoadingType] = useState<null | "call" | "message">(
+    null
+  );
+  const form = useForm<reportTopicForm>({
+    resolver: zodResolver(reportTopicSchema),
     defaultValues: {
       description: "",
       topic: topic ? topic?._id : "",
     },
   });
 
-  const isPending = false;
+  const { mutate, isPending } = useCreateReportTopicMutation();
 
+  const onSubmit = (data: reportTopicForm, type: "call" | "message") => {
+    const finalData = {
+      ...data,
+      type,
+    };
+
+    mutate(finalData, {
+      onSuccess: () => {
+        setFeedback(type);
+        form.reset();
+
+        toast.success(
+          <div
+            style={{
+              display: "flex",
+              alignItems: "flex-start",
+              gap: "12px",
+              fontFamily: "Inter, sans-serif",
+              color: "#111827",
+              fontSize: "14px",
+              lineHeight: "1.4",
+              maxWidth: "360px",
+            }}
+          >
+            <div>
+              <div
+                style={{
+                  fontWeight: 600,
+                  marginBottom: "2px",
+                  fontSize: "14px",
+                }}
+              >
+                Zgłoszenie zapisane
+              </div>
+              <div style={{ opacity: 0.8, fontSize: "13px" }}>
+                Twoje zgłoszenie dla tematu{" "}
+                <span style={{ fontWeight: 500, color: "#111827" }}>
+                  “{topic?.title}”
+                </span>{" "}
+                zostało pomyślnie dodane.
+              </div>
+            </div>
+          </div>,
+          { duration: 2000 }
+        );
+
+        setTimeout(() => {
+          setFeedback(null);
+        }, 1000);
+      },
+    });
+  };
   return (
-    <form className="flex items-center space-x-3 w-1/2 my-auto">
-      <Input
-        placeholder="Notatka (opcjonalnie)"
-        {...register("description")}
-        className="h-11"
-      />
-      <Input type="hidden" value={topic?._id} {...register("topic")} />
-
-      <div className=" flex  items-center pr-1 ">
-        <div>
-          {feedback ? (
-            <div className="flex items-center justify-center h-11 w-[96px] rounded-md  text-green-600 bg-background border">
-              <CircleCheck className="w-6 h-6" />
-            </div>
-          ) : (
-            <div className="flex gap-2 h-11 w-[96px]">
-              <Button
-                type="button"
-                // onClick={onSubmit("call")}
-                disabled={isPending}
-                className="flex items-center justify-center group hover:bg-primary "
-                variant="outline"
-                aria-label="Call"
-              >
-                {isPending ? (
-                  <div className="animate-spin ">
-                    <Phone className="w-4 h-4 text-foreground group-hover:text-secondary-foreground" />
-                  </div>
-                ) : (
-                  <Phone className="w-4 h-4 text-foreground group-hover:text-secondary-foreground" />
-                )}
-              </Button>
-              <Button
-                type="button"
-                // onClick={onSubmit("message")}
-                disabled={isPending}
-                className=" flex items-center justify-center group hover:bg-primary"
-                variant="outline"
-                aria-label="Message"
-              >
-                {isPending ? (
-                  <div className="animate-spin">
-                    <Mail className="w-4 h-4 text-foreground group-hover:text-secondary-foreground" />
-                  </div>
-                ) : (
-                  <Mail className="w-4 h-4 text-foreground group-hover:text-secondary-foreground" />
-                )}
-              </Button>
-            </div>
-          )}
+    <Form {...form}>
+      <form className="flex items-center space-x-3  w-1/2 my-auto">
+        <div className="pt-1.5 pb-1.5  w-full">
+          {/* ------ description ----- */}
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-xs text-foreground">
+                  Notatka
+                </FormLabel>
+                <FormControl>
+                  <Textarea
+                    className="min-h-4"
+                    {...field}
+                    placeholder="Notatka (opcjonalnie)"
+                    rows={2}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* --------- name ------- */}
+          <FormField
+            control={form.control}
+            name="topic"
+            render={({ field }) => (
+              <FormItem hidden={true}>
+                <FormControl>
+                  <Input hidden={true} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
-      </div>
-    </form>
+
+        <DialogFooter className="mt-4 flex space-x-1.5">
+          <AnimatedButton
+            ariaLabel="Send message report"
+            loading={loadingType === "message"}
+            success={feedback === "message"}
+            onClick={() =>
+              form.handleSubmit((data) => onSubmit(data, "message"))()
+            }
+            icon={
+              isPending ? (
+                <Loader2 className="animate-spin" />
+              ) : (
+                <Mail className="w-6 h-6" />
+              )
+            }
+          />
+          <AnimatedButton
+            ariaLabel="Send call report"
+            loading={loadingType === "call"}
+            success={feedback === "call"}
+            onClick={() =>
+              form.handleSubmit((data) => onSubmit(data, "call"))()
+            }
+            icon={<Phone className="w-6 h-6" />}
+          />
+        </DialogFooter>
+      </form>
+    </Form>
   );
 };
 
 export default RegisterTopicForm;
+
+const AnimatedButton = ({
+  icon,
+  onClick,
+  loading,
+  success,
+  ariaLabel,
+}: {
+  icon: JSX.Element;
+  onClick: () => void;
+  loading: boolean;
+  success: boolean;
+  ariaLabel: string;
+}) => {
+  return (
+    <Button
+      variant="outline"
+      type="button"
+      size="icon"
+      onClick={onClick}
+      disabled={loading || success}
+      aria-label={ariaLabel}
+      className={`disabled:opacity-100 relative w-12 h-12 rounded-lg  transition-colors duration-300 cursor-pointer
+        ${success ? "bg-primary text-foreground" : "  text-foreground "}
+      `}
+    >
+      {/* Success Checkmark */}
+      {success && (
+        <CircleCheck className="w-16 h-16 mx-auto text-foreground " />
+      )}
+
+      {/* Default Icon */}
+      {!loading && !success && icon}
+    </Button>
+  );
+};
