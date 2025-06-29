1e51f621121c781e2019dfaf09ce705af85becbe
https://github.com/HurasAdam <huradam@wp.pl>
2025-06-29 20:07:48 +0200

    feat: add product service and use-products hook.

--- src/hooks/products/use-products.ts
+++ src/hooks/products/use-products.ts
@@ -1,5 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import type { ProductForm } from "../../components/product/product-modal";
 import { productsService } from "../../services/products.service";
+
+export const useCreateProductMutation = () => {
+  return useMutation({
+    mutationFn: (data: ProductForm) => productsService.create(data),
+  });
+};
 
 export const useFindProductsQuery = () => {
   return useQuery({
--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,11 +1,17 @@
 import api from "@/config/api.client";
+import type { ProductForm } from "../components/product/product-modal";
 import type { IProduct } from "../types/product";
 const baseUrl = "/products";
+
+const create = (data: ProductForm) => {
+  return api.post(baseUrl, data);
+};
 
 const find = (params?: URLSearchParams): Promise<IProduct[]> => {
   return api.get(baseUrl, { params });
 };
 
 export const productsService = {
+  create,
   find,
 };
