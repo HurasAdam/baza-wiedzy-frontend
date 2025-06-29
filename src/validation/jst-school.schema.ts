382b395018be02582e83f98a39755cb420934fea
https://github.com/HurasAdam <huradam@wp.pl>
2025-06-29 10:06:51 +0200

    feat: add validation jst-school-schema.

---
+++ src/validation/jst-school.schema.ts
@@ -0,0 +1,13 @@
+import z from "zod";
+
+export const jstSchoolSchema = z.object({
+  jstProjectId: z.string().min(1, "Wybierz projekt"),
+  name: z.string().min(3, "Nazwa musi zawierać conajmniej trzy znaki"),
+  email: z.string().email("Błędny adres email"),
+  adres: z.string().min(3, "adres musi zawierać conajmniej 3 znaki"),
+  szId: z.string().min(2, "szId musi zawierac conajmniej 2 znaki"),
+  phone: z
+    .string()
+    .min(2, "telefon kontaktowy musi zawierac conajmniej 9 znaków")
+    .max(12, "telefon może zwierać maksymalnie 12 znaków"),
+});
