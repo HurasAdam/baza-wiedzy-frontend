6c3ccea5bd66592d75de74e5bfec070c946e74cc
https://github.com/HurasAdam <huradam@wp.pl>
2025-06-29 10:05:13 +0200

    style: use muted foreground color for title and description.

--- src/components/shared/EmptyState.tsx
+++ src/components/shared/EmptyState.tsx
@@ -21,8 +21,10 @@ const EmptyState = ({
   return (
     <div className="col-span-full flex flex-col items-center justify-center mt-44 text-center text-muted-foreground">
       {icon ?? <PackageSearch className="w-10 h-10 text-muted" />}
-      <h3 className="text-lg font-semibold text-foreground mb-1">{title}</h3>
-      <p className="text-sm mb-4 max-w-[350px] text-foreground">
+      <h3 className="text-lg font-semibold  mb-1 text-muted-foreground">
+        {title}
+      </h3>
+      <p className="text-sm mb-4 max-w-[350px] text-muted-foreground">
         {description}
       </p>
       {onReset && (
