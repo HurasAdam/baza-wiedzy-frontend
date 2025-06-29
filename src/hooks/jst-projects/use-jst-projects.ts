060147842585a55e45e93695b42e49b5f15f2aa3
https://github.com/HurasAdam <huradam@wp.pl>
2025-06-29 10:03:20 +0200

    feat: add jst-project-service and use-jst-projects hook.

--- src/hooks/jst-projects/use-jst-projects.ts
+++ src/hooks/jst-projects/use-jst-projects.ts
@@ -1,5 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import type { JstProjectForm } from "../../components/jst-project/jst-project-modal";
 import { jstProjectsService } from "../../services/jst-projects.service";
+
+export const useCreateJstProjectMutation = () => {
+  return useMutation({
+    mutationFn: (data: JstProjectForm) => jstProjectsService.create(data),
+  });
+};
 
 export const useFindJstProjectsQuery = () => {
   return useQuery({
--- src/services/jst-projects.service.ts
+++ src/services/jst-projects.service.ts
@@ -1,7 +1,12 @@
 import api from "@/config/api.client";
+import type { JstProjectForm } from "../components/jst-project/jst-project-modal";
 import type { IJstProject } from "../types";
 
 const BASE_URL = "/projects";
+
+const create = async (formData: JstProjectForm) => {
+  return api.post(BASE_URL, formData);
+};
 
 const find = (): Promise<IJstProject[]> => {
   return api.get(BASE_URL);
@@ -9,4 +14,5 @@ const find = (): Promise<IJstProject[]> => {
 
 export const jstProjectsService = {
   find,
+  create,
 };
