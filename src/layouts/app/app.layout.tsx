f307008b258658c8e85a739b167ce2edd427f6f8
https://github.com/HurasAdam <huradam@wp.pl>
2025-07-12 16:01:54 +0200

    style: updated app layout.

--- src/layouts/app/app.layout.tsx
+++ src/layouts/app/app.layout.tsx
@@ -20,7 +20,7 @@ const AppLayout = () => {
         />
 
         <main className="flex-1 overflow-y-auto h-full w-full bg-background scrollbar-custom">
-          <div className="mx-auto container px-2 sm:px-6 lg:px-8 py-0 md:py-8 w-full h-full">
+          <div className="mx-auto container px-2 sm:px-6 lg:px-6 py-0 md:py-8 w-full h-full">
             <Outlet />
           </div>
         </main>
