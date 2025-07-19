ee1f8e84490c91d709511fe51db52785600b3592
https://github.com/HurasAdam <huradam@wp.pl>
2025-07-19 21:36:57 +0200

    feat: wrap app with TooltipProvider for tooltip support.

--- src/providers/react-query-provider.tsx
+++ src/providers/react-query-provider.tsx
@@ -1,16 +1,19 @@
+import queryClient from "@/config/query.client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { Toaster } from "sonner";
-import queryClient from "@/config/query.client";
+import { TooltipProvider } from "../components/ui/tooltip";
 import { ThemeProvider } from "./theme-provider";
 
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <Toaster position="top-center" richColors={true} />
+        <TooltipProvider delayDuration={300}>
+          <Toaster position="top-center" richColors={true} />
 
-        {children}
+          {children}
+        </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
   );
