d177fa684bf0a514e247627318a0fbc3548bf2cf
https://github.com/HurasAdam <huradam@wp.pl>
2025-07-21 22:03:13 +0200

    refactor: replace rejection reason text with modal in rejected article banner.

--- src/pages/article/components/RejectedArticleBanner.tsx
+++ src/pages/article/components/RejectedArticleBanner.tsx
@@ -1,60 +1,64 @@
-import { XCircleIcon, Loader, RefreshCcw } from "lucide-react";
+import { XCircleIcon } from "lucide-react";
+import { useState } from "react";
+
+import ArticleRejectionReasonModal from "../../../components/my-entries/article-rejection-reason-modal";
 import { Button } from "../../../components/ui/button";
 import { Card, CardContent } from "../../../components/ui/card";
+import type { Article } from "../../my-entries/components/MyEntryCard";
 
 interface RejectedArticleBannerProps {
-  rejectionReason?: string;
-  isResubmitting: boolean;
-  onResubmit: () => void;
+  article: Article;
 }
 
 export const RejectedArticleBanner: React.FC<RejectedArticleBannerProps> = ({
-  rejectionReason,
-  isResubmitting,
-  onResubmit,
+  article,
 }) => {
+  const [isRejectionReasonAlertOpen, setIsRejectionReasonAlertOpen] =
+    useState<boolean>(false);
+
+  const onRejectionModalOpen = () => {
+    setIsRejectionReasonAlertOpen(true);
+  };
+  const onRejectonModalClose = () => {
+    setIsRejectionReasonAlertOpen(false);
+  };
+
   return (
-    <Card className="mb-6 border bg-destructive/10 border-destructive/30">
-      <CardContent className="p-4 sm:p-5 grid grid-cols-[auto_1fr_auto] items-center gap-4 sm:gap-6">
-        <div className="flex items-center justify-center">
-          <XCircleIcon className="w-6 h-6 text-red-700" />
-        </div>
+    <>
+      <Card className="mb-6 border bg-destructive/10 border-destructive/30">
+        <CardContent className="p-4 sm:p-5 grid grid-cols-[auto_1fr_auto] items-center gap-4 sm:gap-6">
+          <div className="flex items-center justify-center">
+            <XCircleIcon className="w-6 h-6 text-red-700" />
+          </div>
 
-        <div className="space-y-1">
-          <h2 className="text-base font-semibold text-foreground">
-            Artykuł został odrzucony
-          </h2>
-          <p className="text-sm text-foreground">
-            Artykuł został odrzucony przez moderatora. Zapoznaj się z uwagami i wprowadź poprawki przed ponownym zgłoszeniem.
-          </p>
-          {rejectionReason && (
-           <Button
-           size="sm"
-           className="text-foreground cursor-pointer"
-           variant="link"
-           >
-               Pokaż więcej...
-           </Button>
-          )}
-        </div>
-
-        <div className="flex gap-2">
-          <Button
-            size="sm"
-            variant="ghost"
-            className="flex items-center gap-1 bg-muted hover:bg-primary"
-            onClick={onResubmit}
-            disabled={isResubmitting}
-          >
-            {isResubmitting ? (
-              <Loader className="w-4 h-4 animate-spin" />
-            ) : (
-              <RefreshCcw className="w-4 h-4" />
+          <div className="space-y-1">
+            <h2 className="text-base font-semibold text-foreground">
+              Artykuł został odrzucony
+            </h2>
+            <p className="text-sm text-foreground">
+              Artykuł został odrzucony przez moderatora. Zapoznaj się z uwagami
+              i wprowadź poprawki przed ponownym zgłoszeniem.
+            </p>
+            {article && article.rejectionReason && (
+              <Button
+                onClick={onRejectionModalOpen}
+                size="sm"
+                className="text-foreground cursor-pointer"
+                variant="link"
+              >
+                Pokaż więcej...
+              </Button>
             )}
-            Poproś o ponowną weryfikację
-          </Button>
-        </div>
-      </CardContent>
-    </Card>
+          </div>
+        </CardContent>
+      </Card>
+      {article && article.rejectionReason && (
+        <ArticleRejectionReasonModal
+          article={article}
+          open={isRejectionReasonAlertOpen}
+          setOpen={onRejectonModalClose}
+        />
+      )}
+    </>
   );
 };
