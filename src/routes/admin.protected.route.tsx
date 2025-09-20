09cb47749d7fcc4b4729697e6e71faa754a6fb7c
https://github.com/HurasAdam <huradam@wp.pl>
2025-09-20 22:08:26 +0200

    feat(auth): protect admin route via permissions with toast feedback

--- src/routes/admin.protected.route.tsx
+++ src/routes/admin.protected.route.tsx
@@ -1,21 +1,36 @@
 import { Loader } from "@/components/Loader";
 import { useAuthQuery } from "@/hooks/auth/use-auth";
+import { useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { toast } from "sonner";
 
 const AdminProtectedRoute = () => {
   const { data: authData, isLoading } = useAuthQuery();
+  const [hasShownToast, setHasShownToast] = useState(false);
 
-  if (isLoading) {
-    return <Loader />;
-  }
+  if (isLoading) return <Loader />;
 
-  if (!authData) {
-    return <Navigate to="/auth/login" replace />;
-  }
+  if (!authData) return <Navigate to="/auth/login" replace />;
 
-  const isAdmin = authData?.role?.name?.includes("ADMIN");
+  const canAccessAdminPanel = authData.role?.permissions?.includes("ACCESS_ADMIN_PANEL");
 
-  if (!isAdmin) {
+  if (!canAccessAdminPanel) {
+    if (!hasShownToast) {
+      toast.warning(
+        () => (
+          <div className="flex items-center gap-3">
+            <div className="flex flex-col">
+              <span className="font-medium text-base">Brak uprawnień</span>
+              <span className="text-sm font-normal ">Panel admina dostępny tylko dla uprawnionych użytkowników.</span>
+            </div>
+          </div>
+        ),
+        {
+          duration: 3000,
+        }
+      );
+      setHasShownToast(true);
+    }
     return <Navigate to="/dashboard" replace />;
   }
 
