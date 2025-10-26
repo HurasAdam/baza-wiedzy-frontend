e9face9b6146bf4e1a664657645a652fe3ba056d
https://github.com/HurasAdam <huradam@wp.pl>
2025-10-26 11:45:01 +0100

    feat(ui): update flag modal title and labels

--- src/components/flag/flag-modal.tsx
+++ src/components/flag/flag-modal.tsx
@@ -52,7 +52,7 @@ export const FlagModal = ({ isOpen, setIsOpen, onSave, isLoading }: FlagModalPro
     <Dialog open={isOpen} onOpenChange={setIsOpen} modal>
       <DialogContent className="max-h-[80vh] overflow-y-auto">
         <DialogHeader>
-          <DialogTitle>Dodaj nową flagę</DialogTitle>
+          <DialogTitle>Dodaj nową kolekcję</DialogTitle>
         </DialogHeader>
 
         <Form {...form}>
@@ -67,7 +67,7 @@ export const FlagModal = ({ isOpen, setIsOpen, onSave, isLoading }: FlagModalPro
                   <FormControl>
                     <input
                       {...field}
-                      placeholder="Nazwa flagi"
+                      placeholder="Nazwa kolekcji"
                       className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-primary"
                     />
                   </FormControl>
