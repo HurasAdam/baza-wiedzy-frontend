60b172d07297f950133c72a1d2a29f4e205b012e
HurasAdam <huradam@wp.pl>
2026-01-11 15:50:02 +0100

    feat(jst-project-schema): extend description validation in schema

--- src/validation/jst-project.schema.ts
+++ src/validation/jst-project.schema.ts
@@ -2,5 +2,8 @@ import z from "zod";
 
 export const jstProjectSchema = z.object({
   name: z.string().min(3, "Nazwa musi zawierać conajmniej trzy znaki"),
-  description: z.string().optional(),
+  description: z
+    .string()
+    .min(3, "Opis musi zawierać conajmniej 3 znaki")
+    .max(9000, "Opis projektu nie może przekroczyć 9000 znaków"),
 });
