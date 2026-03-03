d0a3f8ab394948ba64054e74ac86e5e84f4c95da
HurasAdam <huradam@wp.pl>
2026-03-03 20:50:44 +0100

    fix(sidebar): improve sidebar background contrast for dark-slack and dark-supabase themes

--- src/layouts/app/app.layout.tsx
+++ src/layouts/app/app.layout.tsx
@@ -110,7 +110,7 @@ const AppLayout = () => {
               onOpenWorkspaceInviteModal={() => setIsWorkspaceInviteModalOpen(true)}
               onOpenChangeLogModal={() => setIsChangeLogModalOpen(true)}
             />
-            <div className="flex-1 min-h-0 overflow-y-auto scrollbar-custom  rounded-b-2xl py-6">
+            <div className="flex-1 min-h-0 overflow-y-auto scrollbar-custom   rounded-b-2xl py-6">
               <Outlet context={{ onOpenCreateIssueReport: () => setIsCreatingIssueReport(true) }} />
             </div>
           </div>
