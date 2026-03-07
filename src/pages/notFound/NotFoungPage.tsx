86d0382be7e79f21a7cbb74c92bcb5b696f5c019
HurasAdam <huradam@wp.pl>
2026-03-07 22:40:16 +0100

    style: update NoDataFound and NotFoundPage layout

--- src/pages/notFound/NotFoungPage.tsx
+++ src/pages/notFound/NotFoungPage.tsx
@@ -4,7 +4,7 @@ import { NoDataFound } from "../../components/shared/NoDataFound";
 export const NotFoundPage = () => {
   const navigate = useNavigate();
   return (
-    <div style={{ textAlign: "center", padding: "2rem" }}>
+    <div className="max-w-6xl  mx-auto py-9 px-10 " style={{ textAlign: "center", padding: "" }}>
       <NoDataFound
         title="Ups! Strona nie została znaleziona."
         description=" Strona której szukasz nie istnieje."
